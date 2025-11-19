c57d28992b30e833d1971f2182a1e74dbed30a28
emerson sabino <emersonsabino34@gmail.com>
2025-11-19 11:19:09 +0000

    updated

--- app/pedido/page.tsx
+++ app/pedido/page.tsx
@@ -11,16 +11,26 @@ export default function PedidoPage() {
     }
 
     return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Página de Pedido</h1>
 
-        <div className={`w-44 p-5 ${closeModal}`}>
-            <FaCheckCircle />
-            <label className="block mb-2">Nome do Produto:</label>
-            <input type="text" className="border p-2 w-full mb-4" />
-        </div>
+      
+<div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+  <h1 className="text-2xl font-bold mb-6">Página de Pedido</h1>
 
-      <button onClick={handleOpenModal} className="p-2 bg-red-500 text-white">Fazer Pedido</button>
+  <div className={`w-72 p-6 bg-white shadow-md rounded-lg text-center ${closeModal}`}>
+    <FaCheckCircle className="text-green-500 text-4xl mx-auto mb-4" />
+
+    <label className="block text-left font-medium mb-2">
+      Nome do Produto:
+    </label>
+
+    <input
+      type="text"
+      className="border border-gray-300 p-2 w-full rounded-md focus:ring-2 focus:ring-blue-400 focus:outline-none"
+      placeholder="Digite o nome"
+    />
+  </div>
+
+      <button onClick={handleOpenModal} className="p-2 bg-red-500 cursor-pointer hover:text-white transition-all mt-4 text-slate-500 rounded-md">Fazer Pedido</button>
     </div>
   );
 }
