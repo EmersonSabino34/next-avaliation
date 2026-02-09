2d43f4f9985aa5ff7fe60016bd55bacce18d0dcf
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-02-09 22:51:35 +0000

    Add email and password error state management

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,6 +7,7 @@ export default function LoginPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    
     const[emailError, setEmailError] = useState('hidden');
     const [passwordError, setPasswordError] = useState('hidden');
 
